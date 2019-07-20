ccf5064c26557c47753f3cb6f879e0b28c31223f
Peyton Doyle <p6doyle@gmail.com>
2019-07-20 09:59:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,8 +23,8 @@ monopoly = {
 	   :pieces => 4,
 	   :names => {
 	     :reading_railroad => {},
-	     :pennsylvania_railroad => {
-	     }
+	     :pennsylvania_railroad => {},
+	     :b_and_o_railroad => {}
 	   },
 	   :rent_in_dollars => {
 	     :one_piece_owned => 25,

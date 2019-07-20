0a9300774fb3dffdf0a1fbcb14d2d96c29bf4d55
Peyton Doyle <p6doyle@gmail.com>
2019-07-20 09:57:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,8 @@ monopoly = {
 	   :names => {},
 	   :rent_in_dollars => {
 	     :one_piece_owned => 25,
-	     :two_pieces_owned => 50
+	     :two_pieces_owned => 50,
+	     :three_pieces_owned => 100
 	   }
 	   
 	 } 
